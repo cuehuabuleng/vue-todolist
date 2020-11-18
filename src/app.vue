03225a75a828e104d51ed3acdc4c5754d5e1fdcf
jack <1079713849@qq.com>
2020-11-18 20:42:59 +0800

    feat:初始化项目完成相关插件的安装

---
+++ src/app.vue
@@ -0,0 +1,18 @@
+<<template>
+    <div>
+        {{text}}
+    </div>
+</template>
+<script>
+export default {
+    data() {
+        return {
+            text:'123'
+        }
+    },
+}
+</script>
+
+<style lang="stylus" scoped>
+
+</style>
