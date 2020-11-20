4ee2a477d30ff23793e6153cc9fa482ae1fba742
jack <1079713849@qq.com>
2020-11-20 20:31:32 +0800

    feat:webpack基本配置完完成

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import Vue from 'vue'
 import App from './app.vue'
 
+import './assets/style/test.styl'
+
+
 const root = document.createElement('div')
 document.body.appendChild(root);
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,89 @@
 const path = require('path')
 const VueLoaderPlugin = require('./node_modules/vue-loader/lib/plugin')
+const HTMLPlugin = require('html-webpack-plugin')
+const webpack = require('webpack')
+const isDev = process.env.NODE_ENV === 'development'
 
-module.exports = {
+const config = {
+    target: 'web',
     entry: path.join(__dirname, 'src/index.js'),
     output: {
         filename: 'bundle.js',
         path: path.join(__dirname, 'dist')
     },
     plugins: [
-        new VueLoaderPlugin()
+        new VueLoaderPlugin(),
+        new HTMLPlugin(),
+        new webpack.DefinePlugin({
+            'process.env': {
+                NODE_ENV: isDev ? '"development"' : '"production"'
+            }
+        })
     ],
     module: {
         rules: [
             {
                 test: /\.vue$/,
-                use: {
-                    loader: 'vue-loader'
-                }
+                loader: 'vue-loader'
+
+            },
+            {
+                test: /\.jsx$/,
+                loader: 'babel-loader'
+            },
+            {
+                test: /\.styl$/,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    {
+                        loader: 'postcss-loader',
+                        options: {
+                            sourceMap: true
+                        }
+                    },
+                    'stylus-loader'
+                ]
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    'css-loader'
+                ]
+            },
+            {
+                test: /\.(gif|jpg|jpeg|png|svg)$/,
+                use: [
+                    {
+                        loader: 'url-loader',
+                        options: {
+                            limit: 1024,
+                            name: '[name]-aaa.[ext]'
+                        }
+                    }
+                ]
             }
         ]
     }
 }
+
+config.devtool = isDev ? false : '#cheap-module-eval-source-map'
+
+if (isDev) {
+    config.devServer = {
+        port: 8000,
+        host: '0.0.0.0',
+        overlay: {
+            errors: true
+        },
+        // open: true,
+        hot: true
+    }
+    config.plugins.push(
+        new webpack.HotModuleReplacementPlugin(),
+        new webpack.NoEmitOnErrorsPlugin()
+    )
+}
+
+module.exports = config
