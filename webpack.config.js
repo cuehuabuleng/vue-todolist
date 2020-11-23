89f4af807e50e2692daed552cedef98500ffeb65
jack <1079713849@qq.com>
2020-11-23 21:37:48 +0800

    feat:webpack配置css文件单独打包

--- webpack.config.js
+++ webpack.config.js
@@ -3,12 +3,13 @@ const VueLoaderPlugin = require('vue-loader/lib/plugin')
 const HTMLPlugin = require('html-webpack-plugin')
 const webpack = require('webpack')
 const isDev = process.env.NODE_ENV === 'development'
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
 const config = {
     target: 'web',
     entry: path.join(__dirname, 'src/index.js'),
     output: {
-        filename: 'bundle.js',
+        filename: 'bundle.[hash:8].js',
         path: path.join(__dirname, 'dist')
     },
     plugins: [
@@ -31,27 +32,7 @@ const config = {
                 test: /\.jsx$/,
                 loader: 'babel-loader'
             },
-            {
-                test: /\.css$/,
-                use: [
-                    'style-loader',
-                    'css-loader'
-                ]
-            },
-            {
-                test: /\.styl(us)?$/,
-                use: [
-                    'style-loader',
-                    'css-loader',
-                    {
-                        loader: 'postcss-loader',
-                        options: {
-                            sourceMap: true
-                        }
-                    },
-                    'stylus-loader'
-                ]
-            },
+
 
             {
                 test: /\.(gif|jpg|jpeg|png|svg)$/,
@@ -69,9 +50,24 @@ const config = {
     }
 }
 
-config.devtool = isDev ? false : '#cheap-module-eval-source-map'
+config.devtool = isDev ? false : 'source-map'
 
 if (isDev) {
+    // 开发环境的webpack配置
+    config.module.rules.push({
+        test: /\.styl(us)?$/,
+        use: [
+            'style-loader',
+            'css-loader',
+            {
+                loader: 'postcss-loader',
+                options: {
+                    sourceMap: true
+                }
+            },
+            'stylus-loader'
+        ]
+    })
     config.devServer = {
         port: 8000,
         host: '0.0.0.0',
@@ -84,6 +80,36 @@ if (isDev) {
     config.plugins.push(
         new webpack.HotModuleReplacementPlugin(),
         new webpack.NoEmitOnErrorsPlugin()
+    )
+} else {
+    config.output.filename = '[name].[hash:8].js'
+    config.module.rules.push({
+        test: /\.styl(us)?$/,
+        use: [
+            'style-loader',
+            {
+                loader: 'postcss-loader',
+                options: {
+                    sourceMap: true
+                }
+            },
+            {
+                loader: MiniCssExtractPlugin.loader,
+                options: {
+                    publicPath: './'
+                }
+            },
+            'css-loader',
+            'stylus-loader'
+        ],
+        exclude: /node_modules/
+    })
+    config.plugins.push(
+        new MiniCssExtractPlugin({
+            filename: 'css/[name].css',
+            chunkFilename: 'css/[id].css'
+        }),
+        // new ExtractPlugin('style.[contentHash:8].css')
     )
 }
 
